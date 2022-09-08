b227a9efd4b8d73ce6515ec00101993ad979dcfa
atwanzireT <tatwanzire@gmail.com>
2022-09-08 23:42:54 +0300

    state and lifecyle

--- src/components/lifestyle_and_state/lifestyle_state.jsx
+++ src/components/lifestyle_and_state/lifestyle_state.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 
-class Clock extends React.Component{
-    constructor(props){
+class Clock extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = {date: new Date()};    
+        this.state = { date: new Date() };
+    }
+    componentDidMount(){
+        this.timerID = setInterval(
+            () => this.tick(), 2000
+        )
     }
 
-    render(){
-        return(
+    componentWillUnmount(){
+        clearInterval(this.timerID)
+    }
+
+    tick(){
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render() {
+        return (
             <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>      
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         )
     }
