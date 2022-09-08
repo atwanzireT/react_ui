36e87bfae82672aabf566dca7a83ca0faf54beaa
atwanzireT <tatwanzire@gmail.com>
2022-09-08 16:42:20 +0300

    state lifting

---
+++ src/components/state_lifting/state_lifting.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>Water boiling </p>
+    }
+    return <p>Water would not boil</p>
+}
+
+const scalenames = {
+    c: 'Celsius',  
+    f: 'Fahrenheit'
+}
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { temperature: "0"};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ temperature: e.target.value })
+    }
+
+    render() {
+        const temperature = this.state.temperature
+        return (
+            <fieldset>
+                <legend>Enter temperature in Celsius:</legend>
+                <input value={temperature} onChange={this.handleChange} />        
+                <BoilingVerdict celsius={parseFloat(temperature)} />      </fieldset>
+        );
+    }
+}
+export default Calculator
