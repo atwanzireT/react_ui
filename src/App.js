6597a74808c0f3173a80b0b37216e1d956386b39
atwanzireT <tatwanzire@gmail.com>
2022-09-09 19:30:25 +0300

    effect hooks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@
 import './App.css';
 import Calculator from './components/state_lifting/state_lifting';
 import Clock from './components/lifestyle_and_state/lifestyle_state';
+import Example from './components/hooks/hooks_glance';
 
 function App() {
   return (
     <div className="App">
       <Calculator/>
       <Clock/>
+
+      <h1>Intro To Hooks</h1>
+      <h2>State Hooks</h2>
+      <Example/>
     </div>
   );
 }
--- src/components/hooks/hooks_glance.jsx
+++ src/components/hooks/hooks_glance.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Example() {
     // Declare a new state variable, which we'll call "count"
     const [count, setCount] = useState(0)
 
+    // Similar to ComponentDidMount and CompoundDidUpdate
+
+    useEffect(() => {
+        // Update the document title using browser API
+        document.title = `You clicked ${count} times`;
+    })
     return (
         <div>
             <p>You clicked {count} times</p>
